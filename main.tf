5c1a7bf0964a14a386db8b76803b62909a2f41ae
pakunix <paulo.kinos@gmail.com>
2020-01-28 12:12:22 -0300

    Wordpress-GCP

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "google_compute_instance" "helloworld" {
     connection {
       type        = "ssh"
       user        = "paulo_kinos"
-      private_key = file("../id_rsa")
+      private_key = file("id_rsa")
       host        = google_compute_instance.helloworld.network_interface.0.access_config.0.nat_ip
       timeout     = "90s"
     }
@@ -36,7 +36,7 @@ resource "google_compute_instance" "helloworld" {
 
   # Executa a playbook na máquina provisionada
   provisioner "local-exec" {
-    command = "ansible-playbook -i ansible/inventory --private-key ../id_rsa ansible/playbook.yml"
+    command = "ansible-playbook -i ansible/inventory --private-key id_rsa ansible/playbook.yml"
   }
 
 }
--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-  credentials = file("../turma03-infraagil-a-c6896fa2fa23.json")
+  credentials = file("turma03-infraagil-a-c6896fa2fa23.json")
   project     = var.pro_infra
   region      = var.region_c1
 }
