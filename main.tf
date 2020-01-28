730a75b17748c6af01f8e07825ea11c696e63596
Paulo Kinoshita <paulo.kinos@gmail.com>
2020-01-28 11:06:42 -0300

    Wordpress-GCP

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@
 resource "google_compute_instance" "helloworld" {
   name         = "helloworld2"
   machine_type = "f1-micro"
-  zone         = "var.zone_c1a"
+  zone         = var.zone_c1a
 
   # Defini a Imagem da VM
   boot_disk {
     initialize_params {
-      image = "var.lnx_bnt"
+      image = var.lnx_bnt
     }
   }
 
@@ -17,13 +17,35 @@ resource "google_compute_instance" "helloworld" {
     access_config {
     }
   }
-}
-# Cria SQL Gerenciado com Engine MySQL
-resource "google_sql_database_instance" "helloworld" {
-  name             = "database"
-  database_version = "var.sgbd"
-  region           = "var.zone_c1"
-  settings {
-    tier = "db-f1-micro"
+  #Conexão SSH para validar Conectividade
+  provisioner "remote-exec" {
+    inline = ["echo 'Liberado para o Ansible'"]
+
+    connection {
+      type        = "ssh"
+      user        = "paulo_kinos"
+      private_key = file("../id_rsa")
+      host        = google_compute_instance.helloworld.network_interface.0.access_config.0.nat_ip
+      timeout     = "90s"
+    }
   }
+  # Cria o inventário do Ansible
+  provisioner "local-exec" {
+    command = "echo ${google_compute_instance.helloworld.network_interface.0.access_config.0.nat_ip} > ansible/inventory"
+  }
+
+  # Executa a playbook na máquina provisionada
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ansible/inventory --private-key ../id_rsa ansible/playbook.yml"
+  }
+
 }
+## Cria SQL Gerenciado com Engine MySQL
+#resource "google_sql_database_instance" "helloworld" {
+#  name             = "database"
+#  database_version = var.sgbd
+#  region           = var.zone_c1
+#  settings {
+#    tier = "db-f1-micro"
+#  }
+#}
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "zone_c1" {
+  description = "definicao de zona us-central1"
+  default     = "us-central1"
+}
+
+variable "zone_c1a" {
+  description = "definicao de zona us-central1a"
+  default     = "us-central1-a"
+}
+
+variable "region_c1" {
+  description = "definicao de region us-central1a"
+  default     = "us-central1"
+}
+
+variable "pro_infra" {
+  description = "definicao de projeto"
+  default     = "turma03-infraagil-a"
+}
+
+variable "lnx_bnt" {
+  description = "definicao de sistema"
+  default     = "ubuntu-1804-bionic-v20191113"
+}
+
+variable "sgbd" {
+  description = "definicao de banco"
+  default     = "MYSQL_5_7"
+}
