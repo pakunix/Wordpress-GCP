0eef4333d180a33b0a24c8c3efc1f8b5c6f65448
pakunix <paulo.kinos@gmail.com>
2020-01-28 12:19:45 -0300

    Wordpress-GCP

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "turma03-infraagil-a"
-    credentials = "../turma03-infraagil-a-c6896fa2fa23.json"
+    credentials = "turma03-infraagil-a-c6896fa2fa23.json"
   }
 }
